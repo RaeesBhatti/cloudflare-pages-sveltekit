5e0777c049f3223590bbb0fb9c915a39e958663b
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2022-02-28 10:10:25 -0800

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let random: string;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>The random SSR value is {random}</p>
